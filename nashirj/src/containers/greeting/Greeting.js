d03b6d4743899a50a3cd8de92a896eef1f5e8bc5
Nashir Janmohamed <nashirbass@gmail.com>
2025-03-17 01:30:32 -0400

    Start adapting ashutosh's website

---
+++ nashirj/src/containers/greeting/Greeting.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Greeting.css";
+import SocialMedia from "../../components/socialMedia/SocialMedia";
+import Button from "../../components/button/Button";
+import { greeting } from "../../portfolio";
+import { Fade } from "react-reveal";
+import FeelingProud from "./FeelingProud";
+
+export default function Greeting(props) {
+  const theme = props.theme;
+  return (
+    <Fade bottom duration={2000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+          <div className="greeting-text-div">
+            <div>
+              <h1 className="greeting-text" style={{ color: theme.text }}>
+                {greeting.title}
+              </h1>
+              <p
+                className="greeting-text-p subTitle"
+                style={{ color: theme.secondaryText }}
+              >
+                {greeting.subTitle}
+              </p>
+              <SocialMedia theme={theme} />
+              <div className="portfolio-repo-btn-div">
+                <Button
+                  text="See my resume"
+                  newTab={true}
+                  href={greeting.resume_link}
+                  theme={theme}
+                  className="portfolio-repo-btn"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="greeting-image-div">
+            <img
+							alt="jazzy coder"
+							src={require("../../assets/images/bass-computer.webp")}
+						></img>
+            {/* <FeelingProud theme={theme} /> */}
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
